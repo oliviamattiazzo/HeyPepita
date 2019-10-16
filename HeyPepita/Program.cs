dcb622faafcb6b552e7155a0d0ba04bbacf1a030
Olivia Mattiazzo <oli.pmatt@gmail.com>
2019-10-16 14:32:11 +0100

    First commit - again

---
+++ HeyPepita/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeyPepita
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         /* ********** TWITTER ********** */
+         //Dia 1
+         //Conectar com API do twitter e pegar 10 últimos tweets
+         //Desses 10 últimos tweets, selecionar aquele que é o mais recente de bom dia
+         //Verificar padrão de palavras
+         //Guardar:
+         //Ultimo tweet de 'bom dia'
+         //Último tweet AT ALL
+
+         //Dia N
+         //Verificar se há novos tweets
+         //Se houver, verificar se algum deles é de bom dia
+         //Se tiver tweet de 'bom dia', faz update no tweet de 'bom dia'
+         //Se não tiver, faz update no último tweet AT ALL
+
+
+         /* ********** TELEGRAM ********** */
+      }
+   }
+}
